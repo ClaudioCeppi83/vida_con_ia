bcadf2086602d6e5b70db42a28076f051a5688a1
Claudio Ceppi <101994228+ClaudioCeppi83@users.noreply.github.com>
2025-08-06 17:09:46 +0200

    Setting up the deploy in firebase

--- functions/index.js
+++ functions/index.js
@@ -12,8 +12,6 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 
-require('dotenv').config();
-
 const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
